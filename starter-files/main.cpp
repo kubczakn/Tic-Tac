17fba1900d4f6af0eecf4942652253291a58fe5f
kubczakn <kubczakn@umich.edu>
2020-05-15 16:42:46 -0400

    reseting project

--- starter-files/main.cpp
+++ starter-files/main.cpp
@@ -1,18 +1,11 @@
 #include <iostream>
-#include "player.h"
-#include "board.h"
+#include "board.cpp"
 
-void game_intro() {
-    char input;
-    std::cout << "Welcome to Tic-Tac-Toe! Would you like to start a new game Y/N?";
-    std::cin >> input;
-}
+using namespace std;
+
 
 int main() {
     Board b;
     b.draw_board();
-    b.move(2, 0, 'x');
-    b.move(0, 0, 'x');
-    b.move(1, 2, 'x');
-    b.move(1, 0, 'x');
 }
+
